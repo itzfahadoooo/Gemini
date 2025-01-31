03a6e7de07739bc525d66cc17815ca178aa0a4ee
itzfahadoooo <foyindolapo@gmail.com>
2025-01-31 11:31:39 +0100

    Initial commit

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,26 @@
-import "./Sidebar.css"
-import {assets} from "../../assets/assets"
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-        <div className="top">
-            <img className="menu" src={assets.menu_icon} alt="" />
-            <div className="new-chat">
-              <img src={assets.plus_icon} alt="" />
-              <p>New Chat</p>
-            </div>
-            <div>
-              
-            </div>
+      <div className="top">
+        <img className="menu" src={assets.menu_icon} alt="" />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          <p>New Chat</p>
         </div>
-        <div className="bottom">
-
+        <div className="recent">
+          <p className="recent-title">Recent</p>
+          <div className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is React?</p>
+          </div>
+          
         </div>
+      </div>
+      <div className="bottom"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
