646c58072394c760f44834255902bc4c46a68bce
itzfahadoooo <foyindolapo@gmail.com>
2025-01-31 18:35:58 +0100

    Initial commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,7 +6,31 @@ const Main = () => {
       <div className="nav">
         <p>Gemini</p>
         <img src={assets.user_icon} alt="" />
-      </div>
+          </div>
+          <div className="main-container">
+              <div className="greet">
+                  <p><span>Hello, Dev.</span></p>
+                  <p>How can i help you today?</p>
+              </div>
+              <div className="cards">
+                  <div className="card">
+                      <p>Suggest beautiful places for an upcoming trip</p>
+                      <img src={assets.compass_icon} alt="" />
+                  </div>
+                  <div className="card">
+                      <p>Briefly summarize this concept: Urban Planning</p>
+                      <img src={assets.bulb_icon} alt="" />
+                  </div>
+                  <div className="card">
+                      <p>Brainstorm team bonding activities for our work retreat</p>
+                      <img src={assets.message_icon} alt="" />
+                  </div>
+                  <div className="card">
+                      <p>Improve the readability of the following code</p>
+                      <img src={assets.code_icon} alt="" />
+                  </div>
+              </div>
+          </div>
     </div>
   );
 };
