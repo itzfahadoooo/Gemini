01d1eb4688b395afc033cefd4218e591e0a9bbeb
itzfahadoooo <foyindolapo@gmail.com>
2025-02-01 18:53:21 +0100

    Initial commit

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,1 +1,34 @@
-const apiKey="AIzaSyBGZY1OaBPQhoTmgYmxPWHRxlbbowl242M"
+
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+
+const apiKey = "AIzaSyBGZY1OaBPQhoTmgYmxPWHRxlbbowl242M";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash-exp",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run() {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
+  console.log(result.response.text());
+}
+
+run();
