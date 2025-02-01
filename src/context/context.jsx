ad7378bed336a4fd12932e1b3c38348df2e43809
itzfahadoooo <foyindolapo@gmail.com>
2025-02-01 19:19:54 +0100

    Initial commit

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import run from "../config/gemini";
 import { Context } from "./contextProvider";
 
 const ContextProvider = (props) => {
+    const [input, setInput] = useState("");
+    const [reecentPrompt, setRecentPrompt] = useState("");
+
+
+
+
+
     const onSent = async (prompt) => { 
         await run(prompt);
     };
 
-    
+
 
     const contextValue = {};
   return (
