8235aafebe273c40f779ba3977ab50b44010b047
itzfahadoooo <foyindolapo@gmail.com>
2025-02-03 21:13:04 +0100

    Initial commit

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -16,24 +16,28 @@ const ContextProvider = (props) => {
     }, 75 * index);
   };
 
+  const newChat = () => {
+    setLoading(false);
+    setShowResult(false);
+  };
+
   const onSent = async (prompt) => {
     setResultData("");
     setLoading(true);
-      setShowResult(true);
-      let response
-      if (prompt !== undefined) {
-          response = await run(prompt)
-          setRecentPrompt(prompt)
-      }
-      else {
-          setPrevPrompts(prev => [...prev, input])
-          setRecentPrompt(input)
-          response=await run(input)
-      }
+    setShowResult(true);
+    let response;
+    if (prompt !== undefined) {
+      response = await run(prompt);
+      setRecentPrompt(prompt);
+    } else {
+      setPrevPrompts((prev) => [...prev, input]);
       setRecentPrompt(input);
-      setPrevPrompts(prev=>[...prev,input])
+      response = await run(input);
+    }
+    setRecentPrompt(input);
+    setPrevPrompts((prev) => [...prev, input]);
     let responseArray = response.split("**");
-    let newResponse="";
+    let newResponse = "";
     for (let i = 0; i < responseArray.length; i++) {
       if (i === 0 || i % 2 !== 1) {
         newResponse += responseArray[i];
@@ -64,6 +68,7 @@ const ContextProvider = (props) => {
     resultData,
     input,
     setInput,
+    newChat,
   };
 
   return (
